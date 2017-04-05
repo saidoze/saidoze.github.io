f5871bbe1773da0c1d182e796b27a85505e7acf4
saidoze <tnuyttens@hotmail.com>
2017-04-05 18:10:45 +0200

    .

--- app/directives/repeatIsDoneDirective.js
+++
@@ -1,9 +0,0 @@
-leagueRankingApp.directive('repeatIsDone', function() {
-  return function(scope, element, attrs) {
-    //angular.element(element).css('color','blue');
-    if (scope.$last){
-      //window.alert("im the last!");
-      scope.$eval(attrs.repeatIsDone);
-    }
-  };
-});
