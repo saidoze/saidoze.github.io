c6829257783f6108bef44c43e4ebc6654f06c4c7
saidoze <tnuyttens@hotmail.com>
2017-06-07 10:43:20 +0200

    .

--- quickybakkers/app/controllers/rankingController.js
+++ quickybakkers/app/controllers/rankingController.js
@@ -118,6 +118,26 @@ leagueRankingApp.controller('RankingController', function ($scope, $q, $timeout,
           _team1Player1.matchesPlayed -= 1;
         }
 
+        //if gamedateID is... -> filter quicky points (4e speler zonder punten in die match)
+        if(g.$id == '-') {
+          //quicky = -Ke_6QlSCtMQkc5_EAhK - is toevallig altijd player 1
+          _team1Player1.goalsScored -= 9;
+          _team1Player1.amountLose -= 1;
+          _team1Player1.matchesPlayed -= 1;
+        }
+        if(g.$id == '-') {
+          //quicky = -Ke_6QlSCtMQkc5_EAhK - is toevallig altijd player 1
+          _team1Player1.goalsScored -= 6;
+          _team1Player1.amountLose -= 1;
+          _team1Player1.matchesPlayed -= 1;
+        }
+        if(g.$id == '-') {
+          //quicky = -Ke_6QlSCtMQkc5_EAhK - is toevallig altijd player 1
+          _team1Player1.goalsScored -= (9+6+7);
+          _team1Player1.amountLose -= 1;
+          _team1Player1.matchesPlayed -= 1;
+        }
+
       });
 
       _.each($scope.gamedates, function(g) {
